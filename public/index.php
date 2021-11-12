55653e4a4da8ce8b6e4ddb4f23727ad12f6b17ea
Moises Jimenez <MoisesJimenez@Yandex.com>
2021-11-12 17:04:08 -0600

    0.1.0
    -Agregada conexión a base de datos
    -Primeros pasos de Routing

--- public/index.php
+++ public/index.php
@@ -2,9 +2,30 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 $router = new Router();
 
+// Login
+$router->get('/', [LoginController::class, 'login']);
+$router->post('/', [LoginController::class, 'login']);
+$router->get('/', [LoginController::class, 'logout']);
+
+// Crear Cuenta
+$router->get('/crear', [LoginController::class, 'crear']);
+$router->post('/crear', [LoginController::class, 'crear']);
+
+// Reset Password
+$router->get('/olvide', [LoginController::class, 'olvide']);
+$router->post('/olvide', [LoginController::class, 'olvide']);
+
+// Colocar el nuevo password
+$router->get('/reestablecer', [LoginController::class, 'reestablecer']);
+$router->post('/reestablecer', [LoginController::class, 'reestablecer']);
+
+// Confirmacion de Cuenta
+$router->get('/mensaje', [LoginController::class, 'mensaje']);
+$router->get('/confirmar', [LoginController::class, 'confirmar']);
 
 
 
