8a636432ab7e4a220e2c4ac73a2f9a779a71837c
Moises Jimenez <MoisesJimenez@Yandex.com>
2021-11-14 18:35:50 -0600

    0.8.3
    -Agregada verificación para que las 2 contraseñas coincidan

--- models/Usuario.php
+++ models/Usuario.php
@@ -11,6 +11,7 @@ class Usuario extends ActiveRecord{
         $this->nombre = $args['nombre'] ?? '';
         $this->email = $args['email'] ?? '';
         $this->password = $args['password'] ?? '';
+        $this->password2 = $args['password2'] ?? '';
         $this->token = $args['token'] ?? '';
         $this->confirmado = $args['confirmado'] ?? 0;
     }
@@ -19,6 +20,11 @@ class Usuario extends ActiveRecord{
     public function validarNuevaCuenta(){
         if(!$this->nombre) self::$alertas['error'][] = 'Ingresa un Nombre';
         if(!$this->email) self::$alertas['error'][] = 'Ingresa un Email';
+
+        if(!$this->password) self::$alertas['error'][] = 'Ingresa una contraseña';
+        if(strlen($this->password) < 6) self::$alertas['error'][] = 'La contraseña de contener al menos 6 caracteres';
+
+        if($this->password !== $this->password2) self::$alertas['error'][] = 'Las contraselas no coinciden';
 
         return self::$alertas;
     }
