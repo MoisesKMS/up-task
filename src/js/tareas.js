f6c9f0b40a633e677be75919ab14e499dd24e4d6
Moises Jimenez <MoisesJimenez@Yandex.com>
2021-11-22 16:07:47 -0600

    1.2.3
    -Agregados estilos css al modal
    -Animado el modal

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -12,14 +12,17 @@
                 <div class="campo">
                     <label for="tarea" class="tarea">Tarea</label>
                     <input type="text" name="tarea" placeholder="Agrega tarea al Proyecto Actual" id="tarea">
-                    <div class="opciones">
-                        <input type="submit" value="Agregar Tarea" class="submit-nueva-tarea">
-                        <button type="button" class="cerrar-modal">Cancelar</button>
-                    </div> 
                 </div>
+                <div class="opciones">
+                    <input type="submit" value="Agregar Tarea" class="submit-nueva-tarea">
+                    <button type="button" class="cerrar-modal">Cancelar</button>
+                </div> 
             </form>
         `;
-
+        setTimeout(() => {
+            const formulario = document.querySelector('.formulario');
+            formulario.classList.add('animar');
+        }, 100);
         document.querySelector('body').appendChild(modal);
     }
 })();
