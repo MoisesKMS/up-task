73f9ead96a3937eb86fac9a90e27b5b10f9164a3
Moises Jimenez <MoisesJimenez@Yandex.com>
2021-11-24 19:40:12 -0600

    1.5.0
    -Agregada Funcionalidad para Filtrar por tareas completas o pendientes
    -Agregado formulario para filtrar
    -Agregados estilos al formulario para filtrar
    -Agregada funcionalidad para obtener y filtrar tareas dependiendo de lo que el usuario selecciona

--- views/dashboard/proyecto.php
+++ views/dashboard/proyecto.php
@@ -7,9 +7,43 @@
             </button>
         </div>
 
-        <ul id="listado-tareas" class="listado-tareas">
-            
-        </ul>
+        <div class="filtros" id="filtros">
+            <div class="filtros-input">
+                <h2>Filtros:</h2>
+                <div class="campo">
+                    <label for="todas">Todas</label>
+                    <input
+                        type="radio"
+                        id="todas"
+                        name="filtro"
+                        value=""
+                        checked
+                    />
+                </div>
+
+                <div class="campo">
+                    <label for="completadas">Completadas</label>
+                    <input
+                        type="radio"
+                        id="completadas"
+                        name="filtro"
+                        value="1"
+                    />
+                </div>
+
+                <div class="campo">
+                    <label for="pendientes">Pendientes</label>
+                    <input
+                        type="radio"
+                        id="pendientes"
+                        name="filtro"
+                        value="0"
+                    />
+                </div>
+            </div>
+        </div>
+
+        <ul id="listado-tareas" class="listado-tareas"></ul>
     </div>
 
 <?php include_once __DIR__ . '/footer-dashboard.php'; ?>
