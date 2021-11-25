606602bf9958c68395c8a49a77f3b0b86e868520
Moises Jimenez <MoisesJimenez@Yandex.com>
2021-11-25 11:54:25 -0600

    1.6.0
    -Ocultar paneles responsive
    -JS Para interacciones avanzadas
    -CSS Al menú móvil
    -Mostrar y ocultar menú
    -Botón para cerrar sesión

---
+++ src/js/app.js
@@ -0,0 +1,27 @@
+const mobileMenuBtn = document.querySelector('#mobile-menu');
+const cerrarMenuBtn = document.querySelector('#cerrar-menu');
+const sidebar = document.querySelector('.sidebar');
+if (mobileMenuBtn) {
+    mobileMenuBtn.addEventListener('click', function() {
+        sidebar.classList.add('mostrar');
+    });
+}
+
+if (cerrarMenuBtn) {
+    cerrarMenuBtn.addEventListener('click', function() {
+        // sidebar.classList.remove('mostrar');
+        sidebar.classList.add('ocultar');
+        setTimeout(() => {
+            sidebar.classList.remove('mostrar');
+            sidebar.classList.remove('ocultar');
+        }, 300);
+    });
+}
+
+//Elimina la clase de mostrar en un tamaño de tablet
+window.addEventListener('resize', function() {
+    let anchoPantalla = document.body.clientWidth;
+    if (anchoPantalla >= 768) {
+        sidebar.classList.remove('mostrar');
+    }
+});
